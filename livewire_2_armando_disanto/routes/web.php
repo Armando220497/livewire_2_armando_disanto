deddc42c29af4731bc20c023bdb834343ea93061
Armando Di Santo <armando.di.santo@hotmail.com>
2024-10-03 16:37:07 +0200

    update

--- livewire_2_armando_disanto/routes/web.php
+++ livewire_2_armando_disanto/routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 });
 
 // Rotte per l'ArticleController
-Route::get('articles/create', CreateArticle::class)->name('articles.create');
+Route::get('articles/create', [ArticleController::class, 'create'])->name('articles.create');
 Route::get('articles/index', [ArticleController::class, 'index'])->name('articles.index');
 Route::get('articles/show/{article}', [ArticleController::class, 'show'])->name('articles.show');
 Route::get('articles/edit/{article}', EditArticle::class)->name('articles.edit'); // Usa il componente Livewire
